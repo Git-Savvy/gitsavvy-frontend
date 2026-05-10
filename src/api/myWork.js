57bb9b201de610555bd92fcdc901903905db39df
Mshael <shawaf990@gmail.com>
2026-05-11 00:49:35 +0300

    remove token from console

--- src/api/myWork.js
+++ src/api/myWork.js
@@ -1,7 +1,6 @@
 import { mainApi } from "./Axios";
 export const fetchMyWork = async () => {
   const token = localStorage.getItem("token");
-  console.log(token);
   const response = await mainApi.get("/my-work", {
     headers: { Authorization: `Bearer ${token}` },
   });
